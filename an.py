71729c02de94bda2fb407f557cd8a11758bd6647
Bharathiv1403 <128215224+Bharathiv1403@users.noreply.github.com>
2023-09-17 12:16:22 +0530

    class encapsulation

---
+++ an.py
@@ -0,0 +1,17 @@
+class VGLUGcompany:
+    def _init_(self,name,age,id,salary,phone):
+        self.name=name
+        self.age=age
+        self.id=id
+        self.salary=salary
+        self.phone=phone
+
+    def getEmployeeDetails(self):
+        print("name:"+self.name)
+        print("Age:",self.age)
+        print("ID:",self.id)
+        print("Salary:",self.salary)
+        print("Phone:",self.phone)
+
+Vcom=VGLUGcompany("BHARATHI",20,1005,650000,8124814525)
+Vcom.getEmployeeDetails()
